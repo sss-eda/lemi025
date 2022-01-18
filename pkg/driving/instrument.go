6a0e8f36ea325c4a7563f5293c06189f26ff05d1
Stephanus Schoeman <57802955+fanie42@users.noreply.github.com>
2022-01-18 16:09:28 +0200

    going home

--- pkg/driving/instrument.go
+++
@@ -1,3 +0,0 @@
-package driving
-
-// type Instrument
--- pkg/nats/event.go
+++
@@ -1,8 +0,0 @@
-package nats
-
-import "github.com/sss-eda/lemi025/pkg/driving"
-
-type Event struct {
-	Type    driving.EventType `json:"type"`
-	Payload []byte            `json:"payload"`
-}
